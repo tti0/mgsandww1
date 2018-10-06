fd24982eb973036b23af65c6c99e3cb8d46d57c9
tti0 <theodoretucker.linuxdev@gmail.com>
2018-10-06 17:13:34 +0100

    More queryResult code * Renamed from `result` to `queryResult` * Now loads in JSON data for display * Handles images based on JSON `hasPhotos` boolean

--- om-dataset/dataset.js
+++ om-dataset/dataset.js
@@ -4,7 +4,7 @@ var dataset =
     "siteIndex": 1,
     "surname": "Widdowson",
     "firstnames": "Alfred John Harold Ryder",
-    "dob": 1893-12-30,
+    "dob": "1893-12-30",
     "placeOfResidence": "Calthorpe 26, Burford Rd., Whalley Range, Manchester",
     "notesFamily": "Son of Alfred John and Alice Widdowson",
     "MGSstarted": 1906,
@@ -12,12 +12,12 @@ var dataset =
     "notesSchool": null,
     "rank": "Second lieutenant",
     "serviceNumber": null,
-    "dod": 1914-08-25,
-    "aat": 20,
+    "dod": "1914-08-25",
+    "aad": 20,
     "regiment": "2nd Bn., South Lancashire Regiment",
     "burial": [
       {
-        "country": "FR",
+        "country": "France",
         "region": "Nord",
         "place": "Briastre Communal Cemetery",
         "subdivision": "South East portion"
@@ -26,6 +26,16 @@ var dataset =
     "cwg": "https://www.cwgc.org/find-war-dead/casualty/531300",
     "notesMilitary": null,
     "notesExtra": null,
-    "photos": null,
+    "hasPhotos": true,
+    "photos": [
+      {
+        "path": "om-dataset/photos/1/poppies.jpg",
+        "alt": "Poppies"
+      },
+      {
+        "path": "om-dataset/photos/1/cornflower.jpg",
+        "alt": "A cornflower"
+      }
+    ]
   }
 ];
