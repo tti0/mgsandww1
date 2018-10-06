faf34174bccba92dd8b55aeb238c75a74defafb5
tti0 <theodoretucker.linuxdev@gmail.com>
2018-10-06 15:40:23 +0100

    Add (some) query functionality * Result route now checks against our array for a matching entry

--- README.md
+++ README.md
@@ -4,7 +4,9 @@
 
 **Development version is avaliable at https://tti0.github.io/mgsandww1.**
 
-A website with searchable details of Old Mancunians who fought in the First World War, and details of the school's links to the war more generally.
+A website with searchable details of Old Mancunians who died in the First World War, and details of the school's links to the war more generally.
+
+For full information, and contact details, please see the "About" page of the site.
 
 ## Licensing and copyright
 
@@ -12,4 +14,4 @@ This project is licensed under the MIT License (see file `LICENSE` for full term
 
 (c) Copyright 2018 Theodore Tucker (tti0)
 
-The copyright of all images used remains with the original copyright holder.
+The copyright of all images used remains with the original copyright holder or The Manchester Grammar School, unless otherwise stated.
---
+++ om-dataset/dataset.js
@@ -0,0 +1,31 @@
+var dataset =
+[
+  {
+    "siteIndex": 1,
+    "surname": "Widdowson",
+    "firstnames": "Alfred John Harold Ryder",
+    "dob": 1893-12-30,
+    "placeOfResidence": "Calthorpe 26, Burford Rd., Whalley Range, Manchester",
+    "notesFamily": "Son of Alfred John and Alice Widdowson",
+    "MGSstarted": 1906,
+    "MGSleft": 1907,
+    "notesSchool": null,
+    "rank": "Second lieutenant",
+    "serviceNumber": null,
+    "dod": 1914-08-25,
+    "aat": 20,
+    "regiment": "2nd Bn., South Lancashire Regiment",
+    "burial": [
+      {
+        "country": "FR",
+        "region": "Nord",
+        "place": "Briastre Communal Cemetery",
+        "subdivision": "South East portion"
+      }
+    ],
+    "cwg": "https://www.cwgc.org/find-war-dead/casualty/531300",
+    "notesMilitary": null,
+    "notesExtra": null,
+    "photos": null,
+  }
+];
