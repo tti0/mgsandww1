5626d70bb38481991a5b4be11a4f928d001276ff
tti0 <theodoretucker.linuxdev@gmail.com>
2018-10-07 07:29:42 +0100

    Work on doQuery * Add and start work on doQuery view * Add 3 more records to dataset for testing

--- om-dataset/dataset.js
+++ om-dataset/dataset.js
@@ -27,16 +27,96 @@ var dataset =
     "cwg": "https://www.cwgc.org/find-war-dead/casualty/531300",
     "notesMilitary": null,
     "notesExtra": null,
+    "hasPhotos": false,
+  },
+  {
+    "siteIndex": 2,
+    "surname": "MacGregor",
+    "firstnames": "Amyas",
+    "dob": "1892-08-21",
+    "placeOfResidence": "Glencairn, Paines Lane, Pinner, Middlesex",
+    "notesFamily": "Son of Alfred Alexander and Frances Macgregor",
+    "MGSstarted": 1905,
+    "MGSleft": 1911,
+    "notesSchool": null,
+    "rank": "Captain",
+    "serviceNumber": null,
+    "dod": "1916-10-13",
+    "aad": 24,
+    "regiment": "1st London Field Coy., Royal Engineers",
+    "burial": [
+      {
+        "country": "France",
+        "region": "Somme",
+        "place": "Carnoy Military Cemetery",
+        "subdivision": "U.9."
+      }
+    ],
+    "cwg": "https://www.cwgc.org/find-war-dead/casualty/293066/",
+    "notesMilitary": null,
+    "notesExtra": "While at school, he was acting captain in the First London Field Co. R.E.. He won the Military Cross and the Star, and was mentioned in dispatches April 13th 1916.",
+    "hasPhotos": false
+  },
+  {
+    "siteIndex": 3,
+    "surname": "Levi",
+    "firstnames": "Harry",
+    "dob": "1889-07-09",
+    "placeOfResidence": null,
+    "notesFamily": null,
+    "MGSstarted": 1903,
+    "MGSleft": 1905,
+    "notesSchool": null,
+    "rank": "Second lieutenant",
+    "serviceNumber": null,
+    "dod": "1917-11-30",
+    "aad": 28,
+    "regiment": "9th Bn., Royal Fusiliers",
+    "burial": [
+      {
+        "country": "France",
+        "region": "Nord",
+        "place": "Cambrai Memorial, Louverval",
+        "subdivision": "Panel 3 and 4"
+      }
+    ],
+    "cwg": "https://www.cwgc.org/find-war-dead/casualty/1754678",
+    "notesMilitary": "Killed in action.",
+    "notesExtra": null,
     "hasPhotos": true,
     "photos": [
       {
-        "path": "om-dataset/photos/1/poppies.jpg",
-        "alt": "Poppies"
-      },
-      {
-        "path": "om-dataset/photos/1/cornflower.jpg",
-        "alt": "A cornflower"
+        "path": "om-dataset/photos/3/levi.png",
+        "alt": "Harry Levi"
       }
     ]
+  },
+  {
+    "siteIndex": 4,
+    "surname": "Maycock",
+    "firstnames": "Charles Ernest",
+    "dob": "1891-07-20",
+    "placeOfResidence": "15, Lime Grove, Longsight, Manchester",
+    "notesFamily": "Son of Alexander George and Lucy Maycock",
+    "MGSstarted": 1904,
+    "MGSleft": 1907,
+    "notesSchool": null,
+    "rank": "Sergeant",
+    "serviceNumber": null,
+    "dod": "1917-11-30",
+    "aad": 26,
+    "regiment": "12th Bn., Rifle Brigade",
+    "burial": [
+      {
+        "country": "France",
+        "region": "Nord",
+        "place": "Cambrai Memorial, Louverval",
+        "subdivision": "Panel 10 and 11"
+      }
+    ],
+    "cwg": "https://www.cwgc.org/find-war-dead/casualty/1754678",
+    "notesMilitary": null,
+    "notesExtra": "He won a scholarship at the College Of Technology and took his MSc before he was 20. He also won a Schuster Research Scholarship, and Experimental Engineer at Messrs. Hans Renold's Works. He had been in the University O.T.C., and joined up at once. He fought at Loos, Ypres, The Somme, and Cambrai. Killed in action at Cambrai.",
+    "hasPhotos": false
   }
 ];
